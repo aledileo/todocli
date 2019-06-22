db9c143f8d91115bea811ec553bb2520daafba0f
Alejandro Di Leo <ale.dileo17@gmail.com>
2019-06-22 14:34:53 -0300

    Fixed initialCheck, added fse

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,14 +1,26 @@
 const { Command } = require('@oclif/command');
+const eol = require('os').EOL;
 const chalk = require('chalk');
 const { initialCheck, list } = require('../controller/todo-controller');
+
+const checkChar = '\u2713 ';
 
 class List extends Command {
   async run() {
     const { args } = this.parse(List);
     initialCheck();
     const todos = list();
-    this.log(chalk.magenta('Todos:'));
-    todos.forEach(({ todo, done }, i) => this.log(`${i + 1}. [${done ? 'x' : ' '}] ${todo}`));
+    if (todos.length === 0) {
+      this.log(chalk.magenta(`${eol}You don\'t have any todos. Enjoy your day!${eol}`));
+    } else {
+      this.log(chalk.magenta(`${eol}Todos:`));
+      todos.forEach(
+        ({ todo, done }, i) => 
+          !done
+          ? this.log(`  ${i + 1}. ${chalk.green(checkChar)}${todo}`)
+          : this.log(`  ${i + 1}. ${todo}`)
+        );
+    }
   }
 }
 
--- src/controller/todo-controller.js
+++ src/controller/todo-controller.js
@@ -1,26 +1,32 @@
-const fs = require('fs');
+const fse = require('fs-extra');
 const path = require('path');
 const os = require('os');
 
-const todoFile = path.join(os.homedir(), 'checkme', 'todos.json');
+const todoFileDir = path.join(os.homedir(), 'todocli');
+const todoFile = path.join(todoFileDir, 'todos.json');
 let todos = [];
 
 function initialCheck() {
-  if (!fs.existsSync(path.dirname(todoFile))) {
-      fs.mkdirSync(path.dirname(todoFile));
-    } else {
-      todos = JSON.parse(fs.readFileSync(todoFile, { encoding: 'utf-8' }));
-  }
+  if (!fse.existsSync(todoFile)) {
+    fse.ensureFileSync(todoFile);
+    fse.writeJsonSync(todoFile, { todos: todos });
+  } else {
+    todos = fse.readJsonSync(todoFile, { encoding: 'utf-8' }).todos;
+  }   
 };
 
 function saveTodos() {
-  const data = JSON.stringify(todos);
-  fs.writeFileSync(todoFile, data, { encoding: 'utf-8' });
+  fse.writeJsonSync(
+    todoFile,
+    { todos: todos },
+    { encoding: 'utf-8' }
+  );
 }
 
 function add(todo) {
   const newTodo = { todo: todo, done: false };
   todos = [...todos, newTodo];
+  console.log(todos);
   saveTodos();
 }
 
@@ -29,7 +35,12 @@ function list() {
 }
 
 function check(todoIndex) {
-  todos = todos.map((todo, i) => i === todoIndex - 1 ? {...todo, done: !todo.done} : todo);
+  todos = todos.map(
+    (todo, i) => 
+      i === todoIndex - 1 
+      ? {...todo, done: !todo.done}
+      : todo
+  );
   saveTodos();
 }
 
